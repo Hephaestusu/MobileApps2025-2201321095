f578d6199d2ccf69b12483a8c08aadd4c6f01c92
Hephaestus <hakamero@gmail.com>
2025-11-27 17:50:29 +0200

    database

---
+++ app/src/main/java/com/example/taskhep/TaskDao.kt
@@ -0,0 +1,23 @@
+package com.example.taskhep
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Query("SELECT * FROM tasks ORDER BY id DESC")
+    suspend fun getAllTasks(): List<Task>
+
+    @Insert
+    suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+}
